dfa831aad452daac81a8e989ed21af9bbde442f6
UBEDULLA SHAIK <97379715+ube14020905@users.noreply.github.com>
2023-10-17 10:14:43 +0530

    Update sumandavg.java

--- task5/sumandavg.java
+++ task5/sumandavg.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class SumAndAvg {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int n;
 		Scanner s=new Scanner(System.in);
 		System.out.println("Enter the Size of array : ");
