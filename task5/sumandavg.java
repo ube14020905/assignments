ccab642289bd4b1ec6142dcc63865d2c5e24d3f4
UBEDULLA SHAIK <97379715+ube14020905@users.noreply.github.com>
2023-10-17 10:11:48 +0530

    Create sumandavg.java

---
+++ task5/sumandavg.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class SumAndAvg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the Size of array : ");
+		n=s.nextInt();
+		int[] a = new int[n];
+		System.out.println("Enter the elements of array :");
+		int sum=0;
+		for(int i=0;i<n;i++) {
+			a[i]=s.nextInt();
+			sum+=a[i];
+		}
+		System.out.println("The sum of all integers in the array is : "+sum);
+		System.out.println("The Average of all integers in the array is : "+sum/n);
+	}
+
+}
