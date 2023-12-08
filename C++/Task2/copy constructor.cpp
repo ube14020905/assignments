5f7bd0a0b0f34ce29ba451c193dad067b92c8a85
UBEDULLA SHAIK <97379715+ube14020905@users.noreply.github.com>
2023-12-08 11:27:21 +0530

    Create copy constructor.cpp

---
+++ C++/Task2/copy constructor.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Person {
+private:
+    string name;
+    int age;
+
+public:
+    Person(const string& Name, int Age) {
+        name = Name;
+        age = Age;
+    }
+    Person(const Person& copy) {
+        name = copy.name;
+        age = copy.age;
+        cout<<"this is copy Constructor of : "<<name<<endl;
+    }
+    void display() const {
+        cout << "Name: " << name << ", Age: " << age << endl;
+    }
+};
+
+int main() {
+    Person p1("Shaik", 21);
+    Person p2 = p1;
+
+    cout << "Person 1 Information:" << endl;
+    p1.display();
+
+    std::cout << "\nPerson 2 (Copy of P1) Information:" << std::endl;
+    p2.display();
+    return 0;
+}
