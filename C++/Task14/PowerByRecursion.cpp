c6c1ad0d758147ea4efcc8179af18a3dbf5f8f66
UBEDULLA SHAIK <97379715+ube14020905@users.noreply.github.com>
2023-12-28 15:57:55 +0530

    Update PowerByRecursion.cpp

--- C++/Task14/PowerByRecursion.cpp
+++ C++/Task14/PowerByRecursion.cpp
@@ -1,13 +1,14 @@
 #include<iostream>
 using namespace std;
-long calculatePower(long base,long power){
+using ll=long long;
+ll calculatePower(ll base,ll power){
 	if(power==0){
 		return 1;
 	}
 	return (base*calculatePower(base,power-1));
 }
 int main(){
-	long base,power;
+	ll base,power;
 	cout<<"Enter the Base number and Power: ";
 	cin>>base>>power;
 	cout<<power<<" th power of "<<base<<" is "<<calculatePower(base,power)<<endl;
