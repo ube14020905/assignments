24fc71c5f7a16322d7888a5ff0278d376f89500e
UBEDULLA SHAIK <97379715+ube14020905@users.noreply.github.com>
2024-01-08 12:54:53 +0530

    Update OperatorOverloading.cpp

--- C++/Task18/OperatorOverloading.cpp
+++ C++/Task18/OperatorOverloading.cpp
@@ -12,11 +12,30 @@ public:
 	LinearEquation(double a, double b, double c) : x(a), y(b), c(c) {}
 	void display()
 	{
+		if(y<0){
+			cout << x << "x " << y << "y = " << c << endl;
+			return;	
+		}
 		cout << x << "x + " << y << "y = " << c << endl;
 	}
 	LinearEquation operator+(LinearEquation &l2)
 	{
 		return LinearEquation(x + l2.x, y + l2.y, c + l2.c);
+	}
+		LinearEquation operator-(LinearEquation &l2)
+	{
+		return LinearEquation(x - l2.x, y - l2.y, c - l2.c);
+	}
+		LinearEquation operator*(LinearEquation &l2)
+	{
+		return LinearEquation(x * l2.x, y * l2.y, c * l2.c);
+	}
+		LinearEquation operator/(LinearEquation &l2)
+	{
+		if(l2.x==0 || l2.y ==0 || l2.c==0){
+			return LinearEquation(0,0,0);
+		}
+		return LinearEquation(x / l2.x, y / l2.y, c / l2.c);
 	}
 };
 int main()
@@ -26,6 +45,15 @@ int main()
 	LinearEquation res;
 	res = l1 + l2;
 	cout << "Addition of two Linear Equations: ";
+	res.display();
+	cout<<"subtration of two Linear Equations: ";
+	res=l1-l2;
+	res.display();
+	cout<<"Multiplication of two Linear Equations: ";
+	res=l1*l2;
+	res.display();
+	cout<<"Division of two Linear Equations: ";
+	res=l1/l2;
 	res.display();
 	return 0;
 }
