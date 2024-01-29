df99277dc1e23b160ef902be59fb4a15d79af83f
UBEDULLA SHAIK <97379715+ube14020905@users.noreply.github.com>
2024-01-29 14:08:19 +0530

    Update server.cpp

--- C++/Task26/server.cpp
+++ C++/Task26/server.cpp
@@ -3,16 +3,20 @@
 
 #pragma comment(lib, "ws2_32.lib")
 using namespace std;
-int main() {
-    WSADATA wsaData;
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        cerr << "Failed to initialize Winsock"<<endl;
+
+int main()
+{
+    WSADATA data;
+    if (WSAStartup(MAKEWORD(2, 2), &data) != 0)
+    {
+        cerr << "Failed to initialize Winsock" << endl;
         return 1;
     }
 
-    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket == INVALID_SOCKET) {
-        cerr << "Failed to create socket"<<endl;
+    SOCKET server = socket(AF_INET, SOCK_STREAM, 0);
+    if (server == INVALID_SOCKET)
+    {
+        cerr << "Failed to create socket" << endl;
         WSACleanup();
         return 1;
     }
@@ -21,31 +25,36 @@ int main() {
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_addr.s_addr = INADDR_ANY;
     serverAddress.sin_port = htons(12345);
-    if (bind(serverSocket, (sockaddr*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR) {
-        cerr << "Bind failed with error: " << WSAGetLastError() <<endl;
-        closesocket(serverSocket);
+
+    if (bind(server, (sockaddr *)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR)
+    {
+        cerr << "Bind failed with error: " << WSAGetLastError() << endl;
+        closesocket(server);
         WSACleanup();
         return 1;
     }
 
-    if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR) {
-        cerr << "Listen failed with error: " << WSAGetLastError() <<endl;
-        closesocket(serverSocket);
+    if (listen(server, SOMAXCONN) == SOCKET_ERROR)
+    {
+        cerr << "Listen failed with error: " << WSAGetLastError() << endl;
+        closesocket(server);
         WSACleanup();
         return 1;
     }
 
-    cout << "Server listening on port 12345.."<<endl;
+    cout << "Server listening on port 12345.." << endl;
 
-    SOCKET clientSocket;
+    SOCKET client;
     sockaddr_in clientAddress;
     int clientAddressSize = sizeof(clientAddress);
 
-    while (true) {
-        clientSocket = accept(serverSocket, (sockaddr*)&clientAddress, &clientAddressSize);
-        if (clientSocket == INVALID_SOCKET) {
+    while (true)
+    {
+        client = accept(server, (sockaddr *)&clientAddress, &clientAddressSize);
+        if (client == INVALID_SOCKET)
+        {
             cerr << "Accept failed with error: " << WSAGetLastError() << endl;
-            closesocket(serverSocket);
+            closesocket(server);
             WSACleanup();
             return 1;
         }
@@ -53,17 +62,35 @@ int main() {
         cout << "Connection accepted from " << inet_ntoa(clientAddress.sin_addr) << ":" << ntohs(clientAddress.sin_port) << endl;
 
         char buffer[1024];
-        int bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
-        if (bytesRead > 0) {
-            buffer[bytesRead] = '\0';
-            cout << "Received message from client: " << buffer << endl;
+        int bytesRead;
 
-            const char* response = "Hello from server!";
-            send(clientSocket, response, strlen(response), 0);
-        }
-        closesocket(clientSocket);
+        do
+        {
+            bytesRead = recv(client, buffer, sizeof(buffer), 0);
+            if (bytesRead > 0)
+            {
+                buffer[bytesRead] = '\0';
+                cout << "Received message from client: " << buffer << endl;
+                if (buffer == "bye" || buffer == "BYE" || buffer == "Bye" || buffer == "Bye!!")
+                {
+                    break;
+                }
+                const char *response;
+                cout << "Enter response for client: ";
+                cin.getline(buffer, sizeof(buffer));
+                response = buffer;
+                send(client, response, strlen(response), 0);
+                if (response == "bye" || response == "BYE" || response == "Bye" || response == "bye!!!")
+                {
+                    break;
+                }
+            }
+        } while (bytesRead > 0);
+
+        closesocket(client);
     }
-    closesocket(serverSocket);
+
+    closesocket(server);
     WSACleanup();
 
     return 0;
