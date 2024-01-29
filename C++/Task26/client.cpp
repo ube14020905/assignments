46b487bd31deb9bd5d1dc4203201a4691a53079a
UBEDULLA SHAIK <97379715+ube14020905@users.noreply.github.com>
2024-01-29 14:11:13 +0530

    Update client.cpp

--- C++/Task26/client.cpp
+++ C++/Task26/client.cpp
@@ -3,43 +3,65 @@
 
 #pragma comment(lib, "ws2_32.lib")
 using namespace std;
-int main() {
-    WSADATA wsaData;
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        cerr << "Failed to initialize Winsock"<<endl;
+
+int main()
+{
+    WSADATA data;
+    if (WSAStartup(MAKEWORD(2, 2), &data) != 0)
+    {
+        cerr << "Failed to initialize Winsock" << endl;
         return 1;
     }
 
-    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSocket == INVALID_SOCKET) {
-        cerr << "Failed to create socket"<<endl;
+    SOCKET client = socket(AF_INET, SOCK_STREAM, 0);
+    if (client == INVALID_SOCKET)
+    {
+        cerr << "Failed to create socket" << endl;
         WSACleanup();
         return 1;
     }
+
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
-    serverAddress.sin_port = htons(12345);                 
+    serverAddress.sin_port = htons(12345);
 
-    if (connect(clientSocket, (sockaddr*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR) {
+    if (connect(client, (sockaddr *)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR)
+    {
         cerr << "Connection failed with error: " << WSAGetLastError() << endl;
-        closesocket(clientSocket);
+        closesocket(client);
         WSACleanup();
         return 1;
     }
-    const char* message = "Hello from client!";
-    send(clientSocket, message, strlen(message), 0);
-    cout << "Message sent to server: " << message << endl;
 
     char buffer[1024];
-    int bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
-    if (bytesRead > 0) {
-        buffer[bytesRead] = '\0';
-        cout << "Received response from server: " << buffer << endl;
-    }
+    int bytesRead;
 
-    closesocket(clientSocket);
+    do
+    {
+        const char *message;
+        cout << "Enter message for server: ";
+        cin.getline(buffer, sizeof(buffer));
+        message = buffer;
+        send(client, message, strlen(message), 0);
+        if (message == "bye" || message == "BYE" || message == "Bye" || message == "Bye!!")
+        {
+            break;
+        }
+        bytesRead = recv(client, buffer, sizeof(buffer), 0);
+        if (buffer == "bye" || buffer == "BYE" || buffer == "Bye" || buffer == "Bye!!")
+        {
+            break;
+        }
+        if (bytesRead > 0)
+        {
+            buffer[bytesRead] = '\0';
+            cout << "Received response from server: " << buffer << endl;
+        }
 
+    } while (bytesRead > 0);
+
+    closesocket(client);
     WSACleanup();
 
     return 0;
